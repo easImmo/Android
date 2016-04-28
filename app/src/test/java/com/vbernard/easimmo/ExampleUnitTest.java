c796d303ed7f509c3534373441c64e4acd9e7c72
victor <victor1bernard@gmail.com>
2016-04-28 17:06:28 +0200

    mise à jour android studio 2.1

--- app/src/test/java/com/vbernard/easimmo/ExampleUnitTest.java
+++ app/src/test/java/com/vbernard/easimmo/ExampleUnitTest.java
@@ -1,15 +1,42 @@
 package com.vbernard.easimmo;
 
+import android.util.Log;
+
+import com.projet.easimmo.dto.EquipmentStateDTO;
+import com.projet.easimmo.dto.EquipmentTypeDTO;
+import com.projet.easimmo.service.ICallback;
+import com.projet.easimmo.service.manager.ServiceManager;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.lang.System;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void test_services() throws Exception {
+        ServiceManager serviceManager = new ServiceManager();
+        serviceManager.getEquipmentStates(new ICallback<List<EquipmentStateDTO>>() {
+            @Override
+            public void success(List<EquipmentStateDTO> equipmentStateDTO) {
+                System.out.println("TEST");
+                for(EquipmentStateDTO dto : equipmentStateDTO){
+                    System.out.println(dto.getmName());
+                }
+            }
+
+            @Override
+            public void failure(Throwable error) {
+                Log.w("", error.getMessage());
+            }
+        });
     }
 }

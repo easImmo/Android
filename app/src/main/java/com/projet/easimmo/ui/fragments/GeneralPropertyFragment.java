4da8e8298e7107b5399d672c2b41fa87c8e77290
Nicolas Ducom <nicolas.ducom@ext.adeoservices.com>
2016-05-10 17:22:22 +0200

    random changes

--- app/src/main/java/com/projet/easimmo/ui/fragments/GeneralPropertyFragment.java
+++ app/src/main/java/com/projet/easimmo/ui/fragments/GeneralPropertyFragment.java
@@ -1,10 +1,12 @@
 package com.projet.easimmo.ui.fragments;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.projet.easimmo.R;
 import com.projet.easimmo.dto.PropertyDTO;
@@ -16,6 +18,8 @@ public class GeneralPropertyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_general_property, container, false);
+        //Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.anim_toolbar);
+
 
         return v;
     }
@@ -23,6 +27,9 @@ public class GeneralPropertyFragment extends Fragment {
     public GeneralPropertyFragment newInstance(PropertyDTO property) {
         GeneralPropertyFragment f = new GeneralPropertyFragment();
         this.property = property;
+/*        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) getActivity().findViewById(R.id.collapsing_toolbar);
+
+        collapsingToolbar.setTitle(this.property.getmName());*/
         return f;
     }
 

9af3b24bc7e5f41a387ece144f2ce8492e7eb7e3
victor <victor1bernard@gmail.com>
2016-05-12 16:00:35 +0200

    photos slider

---
+++ app/src/main/java/com/projet/easimmo/common/adapter/CustomAdapter.java
@@ -0,0 +1,64 @@
+package com.projet.easimmo.common.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.projet.easimmo.R;
+
+/**
+ * Created by victor on 12/05/2016.
+ */
+public class CustomAdapter extends PagerAdapter {
+
+    Context context;
+    int[] imageId = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+
+    public CustomAdapter(Context context){
+        this.context = context;
+
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        // TODO Auto-generated method stub
+
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+
+        View viewItem = inflater.inflate(R.layout.image_item, container, false);
+        ImageView imageView = (ImageView) viewItem.findViewById(R.id.imageView);
+        imageView.setImageResource(imageId[position]);
+
+        ((ViewPager)container).addView(viewItem);
+
+        return viewItem;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return imageId.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        // TODO Auto-generated method stub
+
+        return view == ((View)object);
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        // TODO Auto-generated method stub
+        ((ViewPager) container).removeView((View) object);
+    }
+
+}
--- app/src/main/java/com/projet/easimmo/ui/activities/EDLDetailActivity.java
+++ app/src/main/java/com/projet/easimmo/ui/activities/EDLDetailActivity.java
@@ -1,11 +1,17 @@
 package com.projet.easimmo.ui.activities;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.TextView;
 
 import com.projet.easimmo.R;
+import com.projet.easimmo.common.adapter.CustomAdapter;
 import com.projet.easimmo.dto.display.EdlDetailDisplayDTO;
+import com.viewpagerindicator.CirclePageIndicator;
+import com.viewpagerindicator.PageIndicator;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,5 +33,16 @@ public class EDLDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         _equipment.setText(" "+detail.getEquipmentDTO().getEquipmentTypeDTO());
         _equipment_state.setText(" "+detail.getAssessmentDTO().getEquipmentStateDTO());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager_image);
+        CirclePageIndicator mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
+
+        PagerAdapter adapter = new CustomAdapter(EDLDetailActivity.this);
+
+        viewPager.setAdapter(adapter);
+
+        mIndicator.setViewPager(viewPager);
     }
+
+
 }
